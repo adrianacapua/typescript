bdeb2f6e39dfa4ebdb3cf6cbcd689f551b3850cd
adriana_capua <adriana.capua@senti.com.ph>
2021-09-16 21:31:31 +0800

    feat: basic data types

---
+++ typescript.js
@@ -0,0 +1,14 @@
+// boolean
+var isCool = true;
+// number
+var age = 56;
+// string
+var eyeColor = 'brown';
+var favouriteQuote = "I'm not old, i'm only " + age;
+// arrays
+var pets = ['cat', 'dog', 'pig'];
+var pets1 = ['lion', 'dragon', 'lizard'];
+// objects
+var wizard = {
+    a: 'John'
+};
---
+++ typescript.ts
@@ -0,0 +1,22 @@
+// boolean
+let isCool: boolean = true;
+
+// number
+let age: number = 56;
+
+// string
+let eyeColor: string = 'brown';
+let favouriteQuote: string = `I'm not old, i'm only ${age}`;
+
+// arrays
+let pets: string[] = ['cat', 'dog', 'pig'];
+let pets1: Array<string> = ['lion', 'dragon', 'lizard'];
+
+// objects
+let wizard: object = {
+  a: 'John'
+}
+
+// null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
